0a31550f448f55ab99ae6bc6b77d3b680d32dde5
Muhammad Tarek <muhammadtarek96@gmail.com>
2017-03-24 22:42:03 +0200

    Cleaning the code
    - Making the main app gain focus whenever the user click on it

--- src/app/app.java
+++ src/app/app.java
@@ -1,11 +1,15 @@
 package app;
 
+import app.components.DropdownField;
 import app.components.InputField;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import java.util.ArrayList;
 
@@ -17,23 +21,15 @@ public class app extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        InputField firstNameField = new InputField("First name", "Text", "");
-        InputField lastNameField = new InputField("Last name", "Text", "");
-        
-        
-
         //App container
         BorderPane app = new BorderPane();
-        app.setCenter(firstNameField.render());
-        app.setLeft(lastNameField.render());
-        
-        System.out.println(firstNameField.getValue());
+        app.addEventFilter(MouseEvent.MOUSE_PRESSED, mouseEvent -> app.requestFocus());
         
         //App Scene
         Scene scene = new Scene(app, 1100, 600);
         primaryStage.setTitle("Auction System!");
         
-        //Imporing CSS Files
+        //Importing CSS Files
         scene.getStylesheets().add("/styles/colors.css");
         importCSS().stream().forEach((cssFile) -> scene.getStylesheets().add("/styles/components/" + cssFile + ".css"));
         
@@ -47,7 +43,6 @@ public class app extends Application {
         //Components CSS
         cssFiles.add("buttons");
         cssFiles.add("cards");
-        cssFiles.add("dropdownMenu");
         cssFiles.add("filter");
         cssFiles.add("fonts");
         cssFiles.add("header");
@@ -55,6 +50,7 @@ public class app extends Application {
         cssFiles.add("photos");
         cssFiles.add("radioButtons");
         cssFiles.add("searchBar");
+        cssFiles.add("scrollbar");
         
         return cssFiles;
     }
