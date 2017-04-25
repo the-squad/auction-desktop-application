3ccd3547483c5e2e1d44f71a4fff8760aa70c2c6
Muhammad Tarek <muhammadtarek96@gmail.com>
2017-04-25 18:00:23 +0200

    Updating MIT header

--- src/models/Buyer.java
+++ src/models/Buyer.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright 2017 Muhammad.
+ * Copyright 2017 Contributors.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -21,6 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+
 package models;
 
 import java.util.ArrayList;
