a769927851fb8e4333fbb640a56d26bf790bd202
Muhammad Tarek <muhammadtarek96@gmail.com>
2017-04-25 15:13:50 +0200

    Adding database.sql

--- src/models/Database.java
+++ src/models/Database.java
@@ -12,10 +12,10 @@ public class Database {
 
     // JDBC driver name and database URL
     //static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://localhost/auctions";
+    static final String DB_URL = Config.DB_URL;
     //  Database credentials
-    static final String USER = "root";
-    static final String PASS = "";
+    static final String USER = Config.USER;
+    static final String PASS = Config.PASS;
 
     private static Database instance = null;
     private Connection connection;
@@ -40,7 +40,7 @@ public class Database {
         }
         return connection;
     }
-    public void closeConnection(){
+    public void closeConnection() {
         try {
             connection.close();
         } catch (SQLException ex) {
@@ -48,62 +48,4 @@ public class Database {
         }
         connection = null;
     }
-
-   /* public static void main(String[] args) {
-        Connection conn = null;
-        Statement stmt = null;
-        try {
-            //STEP 2: Register JDBC driver
-            //Class.forName(JDBC_DRIVER);
-
-            //STEP 3: Open a connection
-            System.out.println("Connecting to database...");
-            conn = Database.getInstance().getConnection();
-
-            //STEP 4: Execute a query
-            System.out.println("Creating statement...");
-            stmt = conn.createStatement();
-            String sql;
-            sql = "SELECT `id`, `name`, `email` FROM `users`";
-            ResultSet rs = stmt.executeQuery(sql);
-
-            //STEP 5: Extract data from result set
-            while (rs.next()) {
-                //Retrieve by column name
-                int id = rs.getInt("id");
-                String name = rs.getString("name");
-                String email = rs.getString("email");
-
-                //Display values
-                System.out.print("ID: " + id);
-                System.out.print(", name: " + name);
-                System.out.println(", email: " + email);
-            }
-            //STEP 6: Clean-up environment
-            rs.close();
-            stmt.close();
-            conn.close();
-        } catch (SQLException se) {
-            //Handle errors for JDBC
-            System.err.println(se.getMessage());
-        } //Handle errors for Class.forName
-        finally {
-            //finally block used to close resources
-            try {
-                if (stmt != null) {
-                    stmt.close();
-                }
-            } catch (SQLException se2) {
-            }// nothing we can do
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException se) {
-                se.printStackTrace();
-            }//end finally try
-        }//end try
-        System.out.println("Goodbye!");
-    }//end main*/
-
 }
