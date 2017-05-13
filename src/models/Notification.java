42eb0fe623f2c61559b2b179a6d13df9fc129a07
Mohamed Al-Rifay <mohamed.abdelattif@fci.helwan.edu.eg>
2017-05-13 23:58:59 +0200

    Finally, notifications worked :dancers:

--- src/models/Notification.java
+++ src/models/Notification.java
@@ -8,6 +8,7 @@ package models;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -23,6 +24,11 @@ public class Notification extends Model<Notification> {
     private int _itemId;
     private int _status;
     private String notificationString;
+
+    @Override
+    public String toString() {
+        return super.toString() + "\n" + this.notificationString;
+    }
 
     public Notification(int _id, int _auctionId, int _itemId, int _status, String notificationString) {
         this._id = _id;
@@ -51,17 +57,56 @@ public class Notification extends Model<Notification> {
         return _status;
     }
 
-    private static ArrayList<Notification> getnewBids(int userId) {
+    public static ArrayList<Notification> getNewBids(int userId) {
         try {
-            String Query = "SELECT `notifications`.`ID`, `notifications`.`auctionId`,(SELECT users.Name FROM users WHERE users.ID = bids.UserID) as Name ,bids.Price , items.Name  as itemName , notifications.status FROM items,notifications JOIN auctions ON notifications.auctionId = auctions.ID JOIN bids ON bids.AuctionID = auctions.ID JOIN users ON users.ID = auctions.UserID WHERE users.ID = ? AND items.ID = auctions.ItemID AND notifications.status = '1'\n" +
-"UNION\n" +
-"SELECT `notifications`.`ID`, `notifications`.`auctionId` , users.Name , bids.Price , items.Name as itemName , notifications.status FROM items, notifications JOIN auctions ON notifications.auctionId = auctions.ID JOIN bids ON bids.AuctionID = auctions.ID JOIN users ON users.ID = bids.UserID WHERE (SELECT COUNT(*) FROM bids WHERE bids.UserID = ? AND bids.AuctionID = auctions.ID) > 0 AND items.ID = auctions.ItemID AND notifications.status = '1'";
-            PreparedStatement excutequery = Model.generateQuery(Query, userId , userId);
-            
+            String Query = "SELECT * FROM (SELECT `notifications`.`ID`, `notifications`.`auctionId`,\n"
+                    + " (SELECT `users`.`Name` FROM `users` JOIN `bids` on `bids`.`UserID` = `users`.`ID` "
+                    + "WHERE `auctions`.`ID` = `bids`.`AuctionID` ORDER BY `bids`.`Price` DESC LIMIT 1) AS `Name`,\n"
+                    + "    (SELECT MAX(`bids`.`Price`) FROM `bids` WHERE `bids`.`AuctionID` = `auctions`.`ID`) AS `Price`,\n"
+                    + "    `items`.`Name` as `itemName` , `notifications`.`status`\n"
+                    + "FROM `notifications`\n"
+                    + "JOIN `auctions` ON `auctions`.`ID` = `notifications`.`auctionId`\n"
+                    + "JOIN `items` ON `items`.`ID` = `auctions`.`ItemID`\n"
+                    + "WHERE `auctions`.`UserID` = ?\n"
+                    + "UNION\n"
+                    + "SELECT `notifications`.`ID`, `notifications`.`auctionId`,\n"
+                    + "(SELECT `users`.`Name` FROM `users` JOIN `bids` on `bids`.`UserID` = `users`.`ID` "
+                    + "WHERE `auctions`.`ID` = `bids`.`AuctionID` ORDER BY `bids`.`Price` DESC LIMIT 1) AS `Name`,\n"
+                    + "(SELECT MAX(`bids`.`Price`) FROM `bids` WHERE `bids`.`AuctionID` = `auctions`.`ID`) AS `Price`,\n"
+                    + "items.Name as itemName , notifications.status\n"
+                    + "FROM `notifications`\n"
+                    + "JOIN `auctions` ON `notifications`.`auctionId` = `auctions`.`ID`\n"
+                    + "JOIN `items` ON `items`.`ID` = `auctions`.`ItemID`\n"
+                    + "WHERE `auctions`.`ID` in (SELECT DISTINCT `auctions`.`ID` FROM `auctions` JOIN `bids` on "
+                    + "`auctions`.`ID` = `bids`.`AuctionID` WHERE `bids`.`UserID` = ?)\n"
+                    + "UNION\n"
+                    + "SELECT\n"
+                    + "	`notifications`.`ID`, `notifications`.`auctionId`,\n"
+                    + "    (SELECT `users`.`Name`\n"
+                    + "         FROM `users` \n"
+                    + "         JOIN `bids` ON bids.UserID = `users`.`ID` \n"
+                    + "         WHERE `bids`.`AuctionID`= `auctions`.`ID`\n"
+                    + "         ORDER BY `bids`.`Price`  LIMIT 1) as `Name`,\n"
+                    + "	(SELECT MAX(`bids`.`Price`) FROM `bids` WHERE `bids`.`AuctionID` = `auctions`.`ID`) AS `Price`,\n"
+                    + "    `items`.`Name` as `itemName`, `notifications`.`status`\n"
+                    + "FROM `notifications`\n"
+                    + "JOIN `subscribe_auctions` ON `notifications`.`auctionId` = `subscribe_auctions`.`AuctionID`\n"
+                    + "JOIN `auctions` ON `auctions`.`ID` = `notifications`.`auctionId`\n"
+                    + "JOIN `items` ON `items`.`ID` = `auctions`.`ItemID`\n"
+                    + "WHERE `subscribe_auctions`.`SubscriberID` = ?)\n"
+                    + "AS `R` WHERE `R`.`status` = '1'";
+            PreparedStatement excutequery = Model.generateQuery(Query, userId, userId, userId);
+
             ResultSet result = excutequery.executeQuery();
             ArrayList<Notification> resultNotification = new ArrayList<>();
+            DecimalFormat df = new DecimalFormat("#");
+            df.setMaximumFractionDigits(2);
             while (result.next()) {
-                resultNotification.add(new Notification(result.getInt("ID"),result.getInt("auctionId"),0, result.getInt("status"), ""));
+                String price = df.format(result.getDouble("Price"));
+                String itemName = result.getString("itemName");
+                String Name = result.getString("Name");
+                String msg = Name + " made a bid of " + price + " on " + itemName;
+                resultNotification.add(new Notification(result.getInt("ID"), result.getInt("auctionId"), 0, result.getInt("status"), msg));
             }
             return resultNotification;
         } catch (SQLException ex) {
@@ -72,13 +117,17 @@ public class Notification extends Model<Notification> {
 
     private static ArrayList<Notification> getWinners(int userId) {
         try {
-            String Query = "SELECT notifications.ID ,items.Name ,notifications.status ,items.ID as itemId  FROM `notifications` JOIN `items` on notifications.itemId = `items`.`ID` JOIN `inventories` ON `inventories`.`ID` = `items`.`InventoryID` WHERE `inventories`.`SellerID` = ? AND `notifications`.`status` = '0'";
+            String Query = "SELECT notifications.ID ,items.Name ,notifications.status ,"
+                    + "items.ID as itemId  FROM `notifications` "
+                    + "JOIN `items` on notifications.itemId = `items`.`ID` "
+                    + "JOIN `inventories` ON `inventories`.`ID` = `items`.`InventoryID` "
+                    + "WHERE `inventories`.`SellerID` = ? AND `notifications`.`status` = '0'";
             PreparedStatement excutequery = Model.generateQuery(Query, userId);
 
             ResultSet result = excutequery.executeQuery();
             ArrayList<Notification> resultNotification = new ArrayList<>();
             while (result.next()) {
-                resultNotification.add(new Notification(result.getInt("ID"), 0, result.getInt("itemId"), result.getInt("status"), ""));
+                resultNotification.add(new Notification(result.getInt("ID"), 0, result.getInt("itemId"), result.getInt("status"), "Congratulations, you have won " + result.getString("Name")));
             }
             return resultNotification;
         } catch (SQLException ex) {
@@ -86,23 +135,73 @@ public class Notification extends Model<Notification> {
         }
         return null;
     }
-    
+
     private static ArrayList<Notification> getStartedAuction(int userId) {
-        
-        return null;
-    }
-    public ArrayList<Notification> getUserNotifications(int userId) {
-        Model.find(Notification.class);
+        try {
+            String Query = "SELECT `notifications`.`ID`, `notifications`.`auctionId`,"
+                    + " `notifications`.`status`, `items`.`Name`\n"
+                    + "FROM `notifications`\n"
+                    + "JOIN `auctions` on `auctions`.`ID` = `notifications`.`auctionId`\n"
+                    + "JOIN `items` ON `items`.`ID` = `auctions`.`ItemID`\n"
+                    + "JOIN `subscribe_auctions` ON `auctions`.`ID` = `subscribe_auctions`.`AuctionID`\n"
+                    + "WHERE `notifications`.`status` = '2' AND `subscribe_auctions`.`SubscriberID` = ?";
+            PreparedStatement excutequery = Model.generateQuery(Query, userId);
+
+            ResultSet result = excutequery.executeQuery();
+            ArrayList<Notification> resultNotification = new ArrayList<>();
+            while (result.next()) {
+                resultNotification.add(new Notification(result.getInt("ID"), result.getInt("auctionId"), 0, result.getInt("status"), "Auction '" + result.getString("Name") + "' Started."));
+            }
+            return resultNotification;
+        } catch (SQLException ex) {
+            Logger.getLogger(Notification.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return null;
     }
 
-    public static void main(String[] args) {
-        System.out.println(Notification.getnewBids(1));
+    public static ArrayList<Notification> getUserNotifications(int userId) {
+        ArrayList<Notification> notifications = new ArrayList<>(getNewBids(userId));
+        notifications.addAll(getWinners(userId));
+        notifications.addAll(getStartedAuction(userId));
+        return notifications;
     }
-    //SELECT `notifications`.`ID`, `notifications`.`auctionId` FROM notifications JOIN auctions ON notifications.auctionId = auctions.ID JOIN bids ON bids.AuctionID = auctions.ID JOIN users ON users.ID = bids.UserID WHERE (SELECT COUNT(*) FROM bids WHERE bids.UserID = 3 AND bids.AuctionID = auctions.ID) > 0
 
-    //SELECT `notifications`.`ID`, `notifications`.`auctionId`,(SELECT users.Name FROM users WHERE users.ID = bids.UserID) as name ,bids.Price , items.Name as itemName FROM items,notifications JOIN auctions ON notifications.auctionId = auctions.ID JOIN bids ON bids.AuctionID = auctions.ID JOIN users ON users.ID = auctions.UserID WHERE users.ID = 1 AND items.ID = auctions.ItemID
-    /*SELECT `notifications`.`ID`, `notifications`.`auctionId`,(SELECT users.Name FROM users WHERE users.ID = bids.UserID) as Name ,bids.Price , items.Name as itemName FROM items,notifications JOIN auctions ON notifications.auctionId = auctions.ID JOIN bids ON bids.AuctionID = auctions.ID JOIN users ON users.ID = auctions.UserID WHERE users.ID = 1 AND items.ID = auctions.ItemID
-UNION
-SELECT `notifications`.`ID`, `notifications`.`auctionId` , users.Name , bids.Price , items.Name as itemName FROM items, notifications JOIN auctions ON notifications.auctionId = auctions.ID JOIN bids ON bids.AuctionID = auctions.ID JOIN users ON users.ID = bids.UserID WHERE (SELECT COUNT(*) FROM bids WHERE bids.UserID = 3 AND bids.AuctionID = auctions.ID) > 0 AND items.ID = auctions.ItemID*/
 }
+
+
+/*
+SELECT `notifications`.`ID`, `notifications`.`auctionId` , 
+	(SELECT `users`.`Name` FROM `users` JOIN `bids` on `bids`.`UserID` = `users`.`ID` WHERE `auctions`.`ID` = `bids`.`AuctionID` ORDER BY `bids`.`Price` DESC LIMIT 1) AS `Name`,
+    (SELECT MAX(`bids`.`Price`) FROM `bids` WHERE `bids`.`AuctionID` = `auctions`.`ID`) AS `Price`,
+    `items`.`Name` as `itemName` , `notifications`.`status`
+FROM `notifications`
+JOIN `auctions` ON `auctions`.`ID` = `notifications`.`auctionId`
+JOIN `items` ON `items`.`ID` = `auctions`.`ItemID`
+WHERE `auctions`.`UserID` = ?
+UNION
+SELECT `notifications`.`ID`, `notifications`.`auctionId`,
+(SELECT `users`.`Name` FROM `users` JOIN `bids` on `bids`.`UserID` = `users`.`ID` WHERE `auctions`.`ID` = `bids`.`AuctionID` ORDER BY `bids`.`Price` DESC LIMIT 1) AS `Name`,
+(SELECT MAX(`bids`.`Price`) FROM `bids` WHERE `bids`.`AuctionID` = `auctions`.`ID`) AS `Price`,
+items.Name as itemName , notifications.status
+FROM `notifications`
+JOIN `auctions` ON `notifications`.`auctionId` = `auctions`.`ID`
+JOIN `items` ON `items`.`ID` = `auctions`.`ItemID`
+WHERE `auctions`.`ID` in (SELECT DISTINCT `auctions`.`ID` FROM `auctions` JOIN `bids` on `auctions`.`ID` = `bids`.`AuctionID` WHERE `bids`.`UserID` = ?)
+UNION
+SELECT
+	`notifications`.`ID`, `notifications`.`auctionId`,
+    (SELECT `users`.`Name`
+         FROM `users` 
+         JOIN `bids` ON bids.UserID = `users`.`ID` 
+         WHERE `bids`.`AuctionID`= `auctions`.`ID`
+         ORDER BY `bids`.`Price`  LIMIT 1) as `Name`,
+	(SELECT MAX(`bids`.`Price`) FROM `bids` WHERE `bids`.`AuctionID` = `auctions`.`ID`) AS `Price`,
+    `items`.`Name` as `itemName`, `notifications`.`status`
+FROM `notifications`
+JOIN `subscribe_auctions` ON `notifications`.`auctionId` = `subscribe_auctions`.`AuctionID`
+JOIN `auctions` ON `auctions`.`ID` = `notifications`.`auctionId`
+JOIN `items` ON `items`.`ID` = `auctions`.`ItemID`
+WHERE `subscribe_auctions`.`SubscriberID` = ?
+
+
+ */
